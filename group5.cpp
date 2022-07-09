08e1751a13d84dd21d915971734675bbbd68f38a
Mcpu3 <mcpu3@outlook.jp>
2022-07-09 17:56:40 +0900

    add group5.h and group5.cpp

--- group5.cpp
+++ group5.cpp
@@ -1,14 +1,531 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <utility>
+#include <vector>
+
 #include "card.h"
 #include "cardset.h"
 #include "player.h"
 #include "gamestatus.h"
 #include "group5.h"
+#include "dealer.h"
+using namespace std;
 
 void Group5::ready() {
+    dealer = Group5Dealer();
+    dealer.regist(new Group5("Me"));
+    dealer.regist(new Group5("Enemy"));
+    dealer.newGame();
+    passed = true;
+    npass = 0;
+    CardSet theDeck = CardSet();
+    theDeck.setupDeck();
+    for (int i = 0; i < theDeck.size(); i++) {
+        if (hand.member(theDeck.at(i))) {
+            dealer.player(0).inHand().insert(theDeck.at(i));
+            dealer.dupHand(0).insert(theDeck.at(i));
+        }
+        else {
+            dealer.player(1).inHand().insert(theDeck.at(i));
+            dealer.dupHand(1).insert(theDeck.at(i));
+        }
+    }
 }
 
 bool Group5::follow(const GameStatus &gstat, CardSet &cs) {
+    vector<pair<CardSet, long double>> cardsetsAndCosts;
+    const vector<CardSet> followableCardset = getFollowableCardsets(gstat, hand);
+    for (const CardSet &cardSet : followableCardset) {
+        cardsetsAndCosts.emplace_back(cardSet, 0.0);
+    }
+    if (dealer.playerInTurn().getName() == "Enemy") {
+        dealer.nextPlayer();
+    }
+    for (pair<CardSet, long double> &cardsetAndCost : cardsetsAndCosts) {
+        cardsetAndCost.second = beamSearch(dealer, cardsetAndCost.first, passed, npass, 0);
+    }
+    sort(cardsetsAndCosts.begin(), cardsetsAndCosts.end(), [](const pair<CardSet, long double> &a, const pair<CardSet, long double> &b) { return a.second < b.second; });
+    cs.insert(cardsetsAndCosts.front().first);
+    hand.remove(cardsetsAndCosts.front().first);
+    return true;
 }
 
 bool Group5::approve(const GameStatus &gstat) {
+    if ((gstat.playerID[gstat.turnIndex] == getId() && dealer.playerInTurn().getName() == "Enemy")
+        || (gstat.playerID[gstat.turnIndex] != getId() && dealer.playerInTurn().getName() == "Me")) {
+        dealer.nextPlayer();
+    }
+    if (passed && dealer.playerInTurnIsLeader()) {
+        dealer.clearDiscardPile();
+    }
+    for (int i = 0; i < gstat.pile.size(); i++) {
+        dealer.playerInTurn().inHand().remove(gstat.pile.at(i));
+    }
+    if (gstat.pile.isEmpty() || !dealer.accept(const_cast<CardSet &>(gstat.pile))) {
+        if (!gstat.pile.isEmpty()) {
+            dealer.putBackOpened(const_cast<CardSet &>(gstat.pile));
+        }
+        passed = true;
+        npass++;
+    }
+    else {
+        passed = false;
+    }
+    if (!passed) {
+        dealer.setAsLeader();
+        npass = 0;
+    }
+    return true;
+}
+
+long double Group5::beamSearch(Group5Dealer dealer, CardSet opened, bool passed, int npass, const int &depth) {
+    if (npass >= dealer.howManyPlayers() * 5) {
+        return 0.0;
+    }
+    if (passed && dealer.playerInTurnIsLeader()) {
+        dealer.clearDiscardPile();
+    }
+    if (opened.isEmpty() || !dealer.accept(opened)) {
+        if (!opened.isEmpty()) {
+            dealer.putBackOpened(opened);
+        }
+        passed = true;
+        npass++;
+    }
+    else {
+        passed = false;
+    }
+    if (dealer.playerInTurn().isEmptyHanded() && dealer.isEmptyHanded(dealer.playerInTurn().getId())) {
+        if (npass >= dealer.howManyPlayers() * 5) {
+            return 0.0;
+        }
+        return evaluate(dealer.gameStatus());
+    }
+    if (!passed) {
+        dealer.setAsLeader();
+        npass = 0;
+    }
+    dealer.nextPlayer();
+
+    vector<pair<CardSet, long double>> cardsetsAndCosts;
+    const vector<CardSet> followableCardset = getFollowableCardsets(dealer.gameStatus(), dealer.playerInTurn().inHand());
+    for (const CardSet &cardSet : followableCardset) {
+        cardsetsAndCosts.emplace_back(cardSet, 0.0);
+    }
+    for (pair<CardSet, long double> &cardsetAndCost : cardsetsAndCosts) {
+        cardsetAndCost.second = beamSearch(dealer, cardsetAndCost.first, passed, npass, depth + 1);
+    }
+    long double cost = accumulate(cardsetsAndCosts.begin(), cardsetsAndCosts.end(), 0.0, [](const long double &accumulation, const pair<CardSet, long double> &cardsetAndCost) { return accumulation + cardsetAndCost.second; }) / cardsetsAndCosts.size();
+    return cost;
+}
+
+long double Group5::evaluate(const GameStatus &gstat) {
+    long double cost = 0.0;
+    for (int i = 0; i < gstat.numParticipants; i++) {
+        if (gstat.playerName[i] == "Me") {
+            cost += gstat.numCards[i];
+        }
+        else {
+            cost -= gstat.numCards[i];
+        }
+    }
+    return cost;
+}
+
+vector<CardSet> Group5::getFollowableCardsets(const GameStatus &gstat, const CardSet &inHand) {
+    vector<CardSet> followableCardsets;
+    vector<CardSet> bucketsOfCardset(14);
+    CardSet cardSet;
+    followableCardsets.emplace_back(cardSet);
+    for (int i = 0; i < inHand.size(); i++) {
+        if (!inHand.at(i).isJoker()) {
+            bucketsOfCardset.at(inHand.at(i).getRank()).insert(inHand.at(i));
+        }
+        else {
+            bucketsOfCardset.front().insert(inHand.at(i));
+        }
+    }
+    for (int i = 1; i < bucketsOfCardset.size(); i++) {
+        if (!bucketsOfCardset.at(i).isEmpty()) {
+            bool isGreaterThan = false;
+            for (int j = 0; j < gstat.pile.size(); j++) {
+                isGreaterThan |= bucketsOfCardset.at(i).at(0).isGreaterThan(gstat.pile.at(j));
+            }
+            if (isGreaterThan) {
+                if (bucketsOfCardset.at(i).size() >= gstat.pile.size()) {
+                    CardSet cardSet;
+                    for (int j = 0; j < gstat.pile.size(); j++) {
+                        cardSet.insert(bucketsOfCardset.at(i).at(j));
+                    }
+                    followableCardsets.emplace_back(cardSet);
+                }
+                if (!bucketsOfCardset.front().isEmpty()) {
+                    if (gstat.pile.size() >= 2 && bucketsOfCardset.at(i).size() >= gstat.pile.size() - 1) {
+                        CardSet cardSet;
+                        for (int j = 0; j < gstat.pile.size() - 1; j++) {
+                            cardSet.insert(bucketsOfCardset.at(i).at(j));
+                        }
+                        cardSet.insert(bucketsOfCardset.front().at(0));
+                        followableCardsets.emplace_back(cardSet);
+                    }
+                }
+            }
+        }
+    }
+    if (!bucketsOfCardset.front().isEmpty()) {
+        CardSet cardSet;
+        cardSet.insert(bucketsOfCardset.front().at(0));
+        followableCardsets.emplace_back(cardSet);
+    }
+    return followableCardsets;
+}
+
+int Group5Dealer::verbose = false;
+
+Group5Dealer::Group5Dealer() {
+    numberOfPlayingPlayers = 0;
+    numberOfParticipants = 0;
+    turn = 0;
+    pauper = 0;
+    for (int i = 0; i < maxNumOfPlayers; i++){
+      for (int j = 0; j <= maxNumOfPlayers; j++) {
+        score[i][j] = 0;
+      }
+      dupHands[i].makeEmpty();
+    }
+    noMillionaire = true;
+    return;
+}
+
+bool Group5Dealer::regist(Player * pl) {
+  //  std::cerr << "### registering of " << pl->playerName() << std::endl;
+  //  std::cerr << "nop=" << numberOfParticipants << ",max=" << maxNumOfPlayers << std::endl;
+  
+  std::cerr << "Registering Player of " << pl->playerName() << " ... ";
+
+    if (numberOfParticipants >= maxNumOfPlayers) {
+      std::cerr << "Error: player registration exceeding the limit (" 
+                << maxNumOfPlayers << ")." << std::endl;
+      return false;
+    }
+    players[numberOfParticipants] = pl;
+    id2player[numberOfParticipants] = pl;
+    pl->setId(numberOfParticipants);
+    numberOfParticipants++;
+    pauper = 0;
+    noMillionaire = true;
+
+    std::cerr << "completed successfully." << std::endl;
+    return true;
+}
+
+Player & Group5Dealer::currentLeader() {
+    return * players[leaderIndex];
+}
+
+bool Group5Dealer::playerInTurnIsLeader() {
+    return leaderIndex == turn;
+}
+
+void Group5Dealer::newGame() {
+    numberOfPlayingPlayers = howManyParticipants();
+    if ( !noMillionaire )
+        pauper = 0;
+    for (int i = 0; i < numberOfParticipants; i++){
+      players[i]->clearHand();
+      //      players[i]->ready();
+      dupHands[i].makeEmpty();
+    }
+    clearDiscardPile();
+    // dealAll();
+    // for (int i = 0; i < numberOfParticipants; i++){
+    //   players[i]->ready();
+    // }
+    setAsLeader();
+}
+
+void Group5Dealer::setAsLeader() {
+    leaderIndex = turn;
+}
+
+void Group5Dealer::setAsLeader(int id) {
+    turn = id;
+    leaderIndex = turn;
+}
+
+bool Group5Dealer::deal(int c) {
+    Card top;
+
+    for (int p = 0; p < numberOfParticipants ; p++) {
+        players[p]->clearHand();
+    }
+    theDeck.setupDeck();
+    theDeck.shuffle();
+    for (int i = 0; i < c; i++) {
+        for (int p = 0; p < numberOfPlayingPlayers; p++) {
+            if ( theDeck.isEmpty() )
+                break;
+            theDeck.draw(top, 0);
+            players[ (numberOfPlayingPlayers - 1 - p) % numberOfPlayingPlayers]->takeCards(top);
+            dupHands[players[(numberOfPlayingPlayers - 1 - p) % numberOfPlayingPlayers]->getId()].insert(top);
+        }
+    }
+    //    turn = 0;
+    return true;
+}
+
+bool Group5Dealer::dealAll() {
+    return deal(53);
+}
+
+void Group5Dealer::clearDiscardPile() {
+    discarded.makeEmpty();
+}
+
+const CardSet & Group5Dealer::discardPile() {
+    return discarded;
+}
+
+bool Group5Dealer::accept(CardSet & opened) {
+        Card openedRank;
+
+    if (! opened.subsetof(dupHands[players[turn]->getId()])) {
+      std::cout << "\t!!! You don't have " << opened
+                << "in your hand of " << dupHands[players[turn]->getId()];
+      return false;
+    }
+
+    if (discarded.isEmpty() && opened.isEmpty() )
+      return false;  // regarded as "pass for empty discard pile."
+
+    if (!discarded.isEmpty() && discarded.size() != opened.size())  // the number of cards must be match. no five cards w/ Jkr allowed.
+      return false;
+    
+    if (!checkRankUniqueness(opened))
+      return false;
+    
+    openedRank = getCardRank(opened);
+
+    if (!discarded.isEmpty()) // 場にカードがでていないのであれば無条件に受理
+      if (!openedRank.isGreaterThan(discardedRank)) 
+        return false;
+    
+
+    // passed all the checks.
+
+    discarded.makeEmpty();
+    discarded.insert(opened);
+    dupHands[players[turn]->getId()].remove(opened);
+    opened.makeEmpty();
+    discardedRank=openedRank;
+
+    return true;
+}
+
+Card Group5Dealer::getCardRank(CardSet & cs){
+    for (int i = 0; i < cs.size(); i++) {
+      if (!cs[i].isJoker()){return cs[i];}
+    }
+    return cs[0];
+}
+
+bool Group5Dealer::checkRankUniqueness(CardSet & cs) {
+    int j = 0;
+    if (cs.size() == 0)
+        return false;
+    if (cs[j].isJoker())
+        j++;
+    for (int i = j+1; i < cs.size(); i++) {
+      if (cs[i].isJoker()){continue;} // 追加：途中にJkrを許す
+      if (cs[j].getNumber() != cs[i].getNumber() )
+        return false;
+    }
+    return true;
+}
+
+void Group5Dealer::showDiscardedToPlayers() {
+    GameStatus gstat = gameStatus();
+    for (int i = 0; i < numberOfPlayingPlayers; i++) {
+        players[(turn + i) % numberOfPlayingPlayers]->approve(gstat);
+    }
+    return; 
+}
+
+void Group5Dealer::withdrawPlayer(int i) {
+    Player * p;
+    p = players[i];
+    for ( ; i+1 < numberOfPlayingPlayers; i++) {
+        players[i] = players[i+1];
+    }
+    if (pauper == numberOfPlayingPlayers) {
+        players[i] = players[i+1];
+        i++;
+        pauper--;
+    }
+    players[i] = p;
+    numberOfPlayingPlayers--;
+    if (numberOfPlayingPlayers > 0) {
+        turn = turn % numberOfPlayingPlayers;
+        leaderIndex = leaderIndex % numberOfPlayingPlayers;
+    }
+}
+
+Player & Group5Dealer::playerInTurnFinished() {
+    //int i;
+    bool bankrupt = false;
+    
+    Player * p = 0;
+    if (numberOfPlayingPlayers == howManyParticipants()
+        // the first finished person is not the millionaire 
+            ) { 
+        if (turn != howManyParticipants() - 1)
+            bankrupt = true;
+        withdrawPlayer(turn); // millionaire
+        if ( bankrupt && !noMillionaire) {
+            pauper = numberOfPlayingPlayers-1;
+            withdrawPlayer(pauper);
+        }
+        // 次の１行をコメントアウトすると 都落ちなし
+        // noMillionaire = false;
+        return *players[pauper];
+    }
+    withdrawPlayer(turn);
+    show();
+    return *p;
+}
+
+
+Player & Group5Dealer::player(int i) {
+    return *players[i];
+}
+
+
+int Group5Dealer::numberOfFinishedPlayers() {
+    return howManyParticipants() - numberOfPlayingPlayers;
+}
+
+Player & Group5Dealer::playerInTurn() {
+    return * players[turn];
+}
+
+Player & Group5Dealer::nextPlayer() {
+    turn = (turn+1) % numberOfPlayingPlayers;
+    return * players[turn];
+}
+
+void Group5Dealer::show() {
+  std::cout << "===========" << std::endl;
+  for (int p = 0; p < numberOfParticipants ; p++) {
+    if ( p==numberOfPlayingPlayers)
+      std::cout << "-----------" << std::endl;
+    if ( p == leaderIndex )
+      std::cout << "* ";
+    else 
+      std::cout << "  ";
+    std::cout << players[p]->toString() << std::endl;
+    if (! players[p]->inHand().equal(dupHands[players[p]->getId()])) {
+      std::cout << "*** error: " << players[p]->getName()
+                << "'s hand is different from the real hand. ***" << std::endl;
+      std::cout << "   (real) : " << dupHands[players[p]->getId()] << std::endl << std::endl;
+    }
+  }
+  std::cout << "===========" << std::endl << std::endl;
+}
+
+void Group5Dealer::putBackOpened(CardSet & opened) {
+  CardSet cs = opened.intersection(dupHands[players[turn]->getId()]);
+  players[turn]->takeCards(cs);
+}
+
+////////////////////////////////////////////////////////////////////////
+void Group5Dealer::updateScore() {
+  int id, n = howManyParticipants();
+  for (int i = 0; i < n; i++) {
+    id = players[n - 1 - i]->getId();
+    // score[id][0] += (n - i);
+    // score[id][0] += (n - i) * (n - i);
+    score[id][0] += (i == 0) ? n + 1 : n - i;
+    score[id][i+1]++;
+  }
+  // score[id][0] += n;
+}
+
+void Group5Dealer::printScore() {
+  std::cout << "### Score ###" << std::endl;
+  for (int i = 0; i < howManyParticipants(); i++) {
+    std::cout << id2player[i]->playerName() << ": "
+              << score[id2player[i]->getId()][0] << std::endl;
+  }
+}
+
+void Group5Dealer::printResult() {
+  int i, j, n = howManyParticipants();
+  int tbl[n];
+  int id, tmp;
+
+  for (i = 0; i < n; i++) tbl[i] = i;
+
+  for (i = n - 1; i > 0; i--) {
+    for (j = 0; j < i; j++) {
+      if (score[id2player[tbl[j]]->getId()][0] < score[id2player[tbl[j+1]]->getId()][0]) {
+        tmp = tbl[j];
+        tbl[j] = tbl[j+1];
+        tbl[j+1] = tmp;
+      }
+    }
+  }
+
+  std::cerr << "### Final Result ###" << std::endl;
+  std::cerr << std::setw(18) << " ";
+  std::cerr << std::setw(10) << "Score  ";
+  for (i = 0; i < n; i++) {
+    std::string s;
+    s = tostr(i+1);
+    s += ((i == 0) ? "st" : ((i == 1) ? "nd" : ((i == 2) ? "rd" : "th")));
+    std::cerr << std::setw(5) << s;
+  }
+  std::cerr << std::endl;
+  std::cerr << "----------------------------";
+  for (i = 0; i < n; i++) {
+    std::cerr << "-----";
+  }
+  std::cerr << std::endl;
+
+  for (i = 0; i < n; i++) {
+    id = id2player[tbl[i]]->getId();
+    std::cerr << (i+1)
+              << ((i == 0) ? "st" : ((i == 1) ? "nd" : ((i == 2) ? "rd" : "th")))
+              << " place: " << std::setw(8) << id2player[tbl[i]]->playerName() << ": "
+              << std::setw(5) << score[id][0] << " : ";
+    for (j = 0; j < n; j++) {
+      std::cerr << std::setw(4) << score[id][j+1] << " ";
+    }
+    std::cerr << std::endl;
+  }
+}
+
+bool Group5Dealer::isEmptyHanded(int playerId) {
+  return dupHands[playerId].isEmpty();
+}
+
+////////////////////////////////////////////////////////////////////////
+GameStatus Group5Dealer::gameStatus(void) const {
+    GameStatus gstat;
+    gstat.pile = discarded;
+    gstat.turnIndex = turn;
+    gstat.numPlayers = numberOfPlayingPlayers;
+    gstat.numParticipants = numberOfParticipants;
+    for (int i = 0; i < howManyParticipants(); i++) {
+        gstat.numCards[i] = players[i]->size();
+        gstat.playerID[i] = players[i]->getId();
+        gstat.playerName[i] = players[i]->playerName();
+    }
+    gstat.leaderIndex = leaderIndex;
+    return gstat;
+}
+
+CardSet &Group5Dealer::dupHand(int i) {
+    return dupHands[i];
 }
--- group5.h
+++ group5.h
@@ -1,14 +1,106 @@
 #ifndef _GROUP5_H_
 #define _GROUP5_H_
 
-using namespace std;
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <vector>
 
+#include "card.h"
+#include "cardset.h"
 #include "player.h"
 #include "gamestatus.h"
+#include "dealer.h"
+using namespace std;
+
+class Group5Dealer {
+ public:
+  static const int NUM_OF_ALL_CARDS = 53;
+  static const int maxNumOfPlayers  = 10;       // 最大プレーヤ数
+  static int verbose;
+
+ private:
+  CardSet theDeck;                    // カードデッキ
+  CardSet discarded;                  // 現在の場のカード
+  Card discardedRank;                 // 現在の場のカードの中の Joker 以外のカードのランク
+                                      // ペアしか考えていないので全部同じ数字
+    
+  Player *players[maxNumOfPlayers];   // プレイヤへのポインタの配列(10人まで)
+                                      // 先頭から，大貧民，... ，大富豪の順
+  int numberOfPlayingPlayers;         // 現在プレイ中のプレイヤの数 (上がったプレイヤは除く)
+  int numberOfParticipants;           // 全プレイヤ数
+  int turn;                           // ターンの回ってきたプレイヤの添字
+  int pauper;                         // 貧民
+  int leaderIndex;                    // リーダとなっているプレイヤの添字
+  bool noMillionaire;                 // 大富豪の搾取を行なわないかどうかのフラグ
+
+  Player *id2player[maxNumOfPlayers]; // プレイヤid からプレイヤを取得するためのテーブル
+  int score[maxNumOfPlayers][maxNumOfPlayers+1];  // プレイヤのスコア
+  CardSet dupHands[maxNumOfPlayers];  // 各プレイヤの手札のコピー(チェック用)
+
+public:
+  Group5Dealer();
+
+  void newGame();
+  bool regist(Player *);              // プレイヤの登録
+  bool deal(int);                     // 引数で与えた枚数のカードを配り(*)，turn をリセット
+  bool dealAll();                     // 各プレイヤに手札を配布
+  void show();                        // 各プレイヤの状況の表示
+    
+  int howManyPlayers() const { return numberOfPlayingPlayers; }    // numberOfPlayers のアクセサ
+  int howManyParticipants() const { return numberOfParticipants; } // numberOfParticipantes のアクセサ
+        
+  void showDiscardedToPlayers();      // 各プレイヤに approve
+    
+  bool accept(CardSet &opened);       // プレイヤが出したカード(opend)が，場のカードに対し
+                                      // て合法かどうかの判定
+
+  bool checkRankUniqueness(CardSet &);  // 引数のカードセットが同じ数字のペアになっているかど
+                                        // うかのチェック(Jokerは任意の数だとみなす)
+                                        // true: OK，false: ダメ
+  Card getCardRank(CardSet &);          // 引数のカードセットの中で，最初にみつかった Joker
+                                        // 以外のカードを返す．Joker しかない場合やカードセッ
+                                        // トが空集合のときは，cs[0] を返す．CardSet内で
+                                        // operator[] が配列のように定義されている
+
+  const CardSet &discardPile();         // 現在の場(discarded) のアクセサ
+  void clearDiscardPile();              // 現在の場(discarded) を空にする
+  bool playerInTurnIsLeader();          // 現在の手番のプレイヤがリーダかどうかの判定
+  void setAsLeader();                   // 現在の手番のプレイヤをリーダとする
+  void setAsLeader(int);                // 引数のプレイヤをリーダとする
+  Player &player(int);                  // 引数のプレイヤのアクセサ
+  int numberOfFinishedPlayers();        // 上がったプレイヤ数
+  void withdrawPlayer(int);             // 引数のプレイヤをゲームから除く(上がったので)
+
+  Player &playerInTurnFinished();       // 現在の手番のプレイヤの上がり処理
+    
+  Player &playerInTurn();               // 現在の手番のプレイヤのアクセサ
+  Player &nextPlayer();                 // 次の手番のプレイヤのアクセサ＋手番の更新
+  Player &currentLeader();              // 現在のリーダの取得
+    
+  void putBackOpened(CardSet &);        // カードの返却(合法手でなかった場合に使用)
+
+  GameStatus gameStatus(void) const;    // gamestatus のアクセサ
+
+  ///////////////
+  void updateScore();                   // スコアの更新
+  void printScore();                    // 各プレイヤのスコアの表示
+  void printResult();                   // 最終結果の表示
+
+  bool isEmptyHanded(int);              // 引数のプレイヤの手札が空集合かどうかの判定
+
+  CardSet &dupHand(int);
+
+};
 
 class Group5 : public Player {
+protected:
+    Group5Dealer dealer;
+    bool passed;
+    int npass;
+
 public:
-    Group5(const char *name = "Group5") : Player(name) {}
+    Group5(const char *name = "Group5") : Player(name), dealer(), passed(true), npass(0) {}
 
 public:
     ~Group5() {}
@@ -21,6 +113,15 @@ public:
 
 public:
     bool approve(const GameStatus &);
+
+protected:
+    long double beamSearch(Group5Dealer, CardSet, bool, int, const int &);
+
+protected:
+    long double evaluate(const GameStatus &);
+
+protected:
+    vector<CardSet> getFollowableCardsets(const GameStatus &, const CardSet &);
 };
 
 #endif
